bf7f7b9fba1a2ba9173eea94c48584e1d007962f
stamin4 <113454963+stamin4@users.noreply.github.com>
2024-01-25 20:26:05 -0300

    Create ejercicio1.cs

---
+++ 3. Condicionales/ejercicio1.cs
@@ -0,0 +1,22 @@
+// 1. Hacer un programa para ingresar un número y luego se emita por pantalla un cartel aclaratorio si “Es mayor a 10” o “No es mayor a 10”.
+
+using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Ingrese un número: ");
+            n = int.Parse(Console.ReadLine());
+
+            if(n > 10)
+                Console.WriteLine("Es mayor a 10");
+            else
+                Console.WriteLine("No es mayor a 10");
+        }
+    }
+}
