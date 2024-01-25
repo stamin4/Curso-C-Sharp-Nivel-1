f222335dccbe71f64998496c924b8246d2235751
stamin4 <113454963+stamin4@users.noreply.github.com>
2024-01-25 19:49:12 -0300

    Update primerPrograma.cs

--- primerPrograma.cs
+++ primerPrograma.cs
@@ -1,3 +1,6 @@
+// 1. Para este primer ejercicio deberás crear tu primer programa que emita en pantalla "Hola Mundo".
+// Adicionalmente, podés hacer las modificaciones correspondientes para que el programa emita "Bienvenido Juanito", donde "Juanito" puede ser tu nombre. 
+
 using System;
 
 namespace primerPrograma
