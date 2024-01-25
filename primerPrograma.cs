116b9dd48677d0f54e28d452bf35c2b43f68b705
stamin4 <113454963+stamin4@users.noreply.github.com>
2024-01-25 19:42:43 -0300

    Create primerPrograma.cs

---
+++ primerPrograma.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace primerPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello stamin4!");
+        }
+    }
+}
