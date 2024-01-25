0bb9f9a33a9aaf7417fe1166e75df7e5df969080
stamin4 <113454963+stamin4@users.noreply.github.com>
2024-01-25 20:45:57 -0300

    Add files via upload

---
+++ 4. Condicionales++/ejercicio2.cs
@@ -0,0 +1,44 @@
+﻿// 2. Un importante negocio de desinfectante líquido realiza descuentos dependiendo de la cantidad de litros vendidos según la siguiente escala:
+
+//          Si vende menos de 100 litros, no hay descuento.
+//          Si vende entre 101 y 300 litros, el descuento es del 10%.
+//          Si vende entre 301 y 500 litros, el descuento es del 15%.
+//          Finalmente, si la venta es de más de 500 litros, el descuento es del 25%.
+
+// Hacer un programa que solicite el ingreso del importe total de la venta y la cantidad de litros vendidos y calcule y emita el importe con el descuento aplicado.
+
+using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float imp,l,final,desc;
+
+            Console.WriteLine("Ingrese la cantidad de litros vendidos:");
+            l = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el importe:");
+            imp = float.Parse(Console.ReadLine());
+
+            if(l <= 100)
+                final = imp;
+            else if(l>100 && l<=300){
+                desc = imp*10/100;
+                final = imp-desc;
+            }
+            else if(l>300 && l<=500){
+                desc = imp*15/100;
+                final = imp-desc;
+            }
+            else{
+                desc = imp*25/100;
+                final = imp-desc;
+            }
+
+            Console.WriteLine("El importe final es de $" + final.ToString("0.00"));    
+
+        }
+    }
+}
---
+++ 4. Condicionales++/ejercicio4.cs
@@ -0,0 +1,24 @@
+﻿// 4. Hacer un programa para ingresar tres números y emitir un cartel aclaratorio si la suma de los dos primeros es mayor al producto del segundo con el tercero.
+
+using System;
+
+namespace ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;
+
+            Console.WriteLine("Ingrese el primer número:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el último número:");
+            c = int.Parse(Console.ReadLine());
+
+            if((a + b) > (b * c))
+                Console.WriteLine("La suma de los dos primeros es mayor al producto del segundo con el tercero.");
+        }
+    }
+}
