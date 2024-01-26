2b2c52a7cb93ed55601fb8c4c2ad99bb614d3211
stamin4 <113454963+stamin4@users.noreply.github.com>
2024-01-26 13:03:03 -0300

    Add files via upload

---
+++ 5. Ciclos/ejercicio_11.cs
@@ -0,0 +1,26 @@
+﻿// Hacer un programa que solicite la edad de un grupo de personas. 
+// El programa deberá pedir edades hasta que se ingrese una edad menor a 18 y deberá mostrar por pantalla cuántas personas de 18 años o más se registraron.
+
+using System;
+
+namespace ejercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad,cont=0;
+
+            Console.WriteLine("Ingrese la edad:");
+            edad = int.Parse(Console.ReadLine());
+
+            while(edad>18){
+                cont++;
+                Console.WriteLine("Ingrese la edad:");
+                edad = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Se registraron " + cont + " personas mayores de 18 años");
+        }
+    }
+}
---
+++ 5. Ciclos/ejercicio_16.cs
@@ -0,0 +1,49 @@
+﻿// Hacer un programa que solicite una lista de números que corta cuando se ingresa un cero. 
+// Luego mostrar la cantidad de números primos, la cantidad de números pares, positivos y negativos
+
+using System;
+
+namespace ejercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,contPares=0,contPos=0,contNeg=0,contPrimos=0,d;
+
+            Console.WriteLine("Ingrese un número:");
+            n = int.Parse(Console.ReadLine());
+
+            while(n!=0){
+
+                if(n<0)
+                    contNeg++;
+                else 
+                    contPos++;
+
+                if(n%2==0)
+                    contPares++;
+
+                d=0;
+                
+                for(int i=1;i<=n;i++){
+                    if(n%i==0)
+                        d++;
+                }
+
+                if(d==2)
+                    contPrimos++;
+
+
+                Console.WriteLine("Ingrese un número:");
+                n = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Números primos: " + contPrimos);
+            Console.WriteLine("Números pares: " + contPares);
+            Console.WriteLine("Números positivos: " + contPos);
+            Console.WriteLine("Números negativos: " + contNeg);
+        }
+    }
+}
