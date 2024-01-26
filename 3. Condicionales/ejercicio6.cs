834825c0b5d8bedd6d9c7c633d45b85424f5e319
stamin4 <113454963+stamin4@users.noreply.github.com>
2024-01-26 12:34:44 -0300

    Add files via upload

---
+++ 3. Condicionales/ejercicio6.cs
@@ -0,0 +1,22 @@
+﻿// Hacer un programa que permita ingresar un número y determinar si el mismo es par o impar. 
+
+using System;
+
+namespace ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            
+            Console.WriteLine("Ingrese un número:");
+            n = int.Parse(Console.ReadLine());
+
+            if(n%2 == 0)
+                Console.WriteLine("El número " + n + " es par");
+            else
+                Console.WriteLine("El número " + n + " es impar");
+        }
+    }
+}
